afa0954ea5723ad8184b18a2746dab581f272f2b
Eric Torstenson <eric.s.torstenson@vumc.org>
2025-05-27 15:32:15 -0500

    Various fixes for first time running with the id lister script

--- src/unwrangle/support/dewrangle.py
+++ src/unwrangle/support/dewrangle.py
@@ -2,15 +2,15 @@
 
 
 from graphql import print_ast
+import sys
 from gql.transport.aiohttp import AIOHTTPTransport
 from gql import Client, gql
 import requests
-from . import _support_details 
+from . import _support_details, descriptor_upsert
 
 class Dewrangler:
     def __init__(self, token, dwrest="https://dewrangle.com/api/rest", gqurl="https://dewrangle.com/api/graphql" ):
-        self.client = None
-
+        self._client = None
 
         assert token is not None, "Dewrangle must be given a valid dewrangle token!"
         # Token associated with the dewrangle client access
@@ -19,33 +19,36 @@ class Dewrangler:
         self.dwrest = dwrest        # REST Api URL 
 
         # GraphQL query associated with gathering studies we can access
-        self.study_query = None
+        self._study_query = None
+        self._descriptor_upsert = None
 
         self.studies = None 
 
+    @property
     def study_query(self):
-        if self.study_query is None:
-            self.study_query = gql((_support_details / "query_studies.gql").read_text())
-        return self.study_query
+        if self._study_query is None:
+            self._study_query = gql((_support_details / "query_studies.gql").read_text())
+        return self._study_query
 
+    @property
     def descriptor_upsert(self):
-        if self.descriptor_upsert is None:
-            self.descriptor_upsert = gql((_support_details / "descriptor_upsert.gql").read_text())
-        return self.descriptor_upsert
+        if self._descriptor_upsert is None:
+            self._descriptor_upsert = gql((_support_details / "descriptor_upsert.gql").read_text())
+        return self._descriptor_upsert
     
     def client(self):
-        if self.client is None:
+        if self._client is None:
             headers = {
                 "x-api-key": self.token
             }
-            transport = AIOHTTPTransport(url=self.gqlurl, headers=headers)
+            transport = AIOHTTPTransport(url=self.gqurl, headers=headers)
 
-            self.client =  Client(
+            self._client =  Client(
                 transport=transport,
                 fetch_schema_from_transport=True,
                 execute_timeout=30,
             )
-        return self.client 
+        return self._client 
     
     def study_list(self):
         if self.studies is None:
@@ -75,16 +78,35 @@ class Dewrangler:
                         "name": node["name"],
                         "globalId": node["globalId"],
                     }
-
         return self.studies 
     
-    def list_descriptors(self, study_global_id):
+    def study_details(self, study_name, org_name=None):
+        studies = self.study_list()
+        if org_name is None:
+            if len(studies) > 1:
+                org_list = ', '.join(studies)
+                sys.stderr.write(f"More than one organization found ({org_list}). Please specify which organization the study belongs to. \n")
+                sys.exit(1)
+            else:
+                org_name = list(self.studies.keys())[0]
+        
+        if study_name not in studies[org_name]['studies']:
+            sys.stderr.write(f"No matching study found for '{study_name}' in organization, '{org_name}'\n")
+            study_list = "\n\t* ".join(studies[org_name]['studies'].keys())
+            sys.stderr.write(f"Available study names include: \n\t* {study_list}\n")
+            sys.exit(1)
+
+        return studies[org_name]['studies'][study_name]
+
+        
+    
+    def list_descriptors(self, study_id):
         headers = {"x-api-key": self.token, "accept": "application/json"}
-        rest_url = f"{self.dwrest}/studies/{study_global_id}/global-descriptors?descriptors=all"
+        rest_url = f"{self.dwrest}/studies/{study_id}/global-descriptors?descriptors=all"
         response = requests.get(rest_url, headers=headers, timeout=None)
         return response.json()
     
-    def update_descriptors(self, study_global_id, descriptors):
+    def update_descriptors(self, study_id, descriptors):
         """
         Uploads the descriptors to the rest API and then gets the updated version via a graphQL call
 
@@ -100,20 +122,26 @@ class Dewrangler:
         }
         # pdb.set_trace()
         resp = requests.post(
-            f"https://dewrangle.com/api/rest/studies/{study_global_id}/files/descriptors.json",
+            f"https://dewrangle.com/api/rest/studies/{study_id}/files/descriptors.json",
             headers=headers,
             json=descriptors,
             timeout=None,
         )
         fileid = resp.json()["id"]
-        variables = {"input": {"studyFileId": fileid, "skipUnavailableDescriptors": True}}
+
+        variables = {"input": {"studyId": study_id, 
+                       "studyFileIds": [{ "id": fileid}], "skipUnavailableDescriptors": True}}
+        
         response = self.client.execute(descriptor_upsert, variable_values=variables)
 
         job_output = response["globalDescriptorUpsert"]["job"]
+        job_id = job_output['id']
+
+        resp = requests.get(f"https://dewrangle.com/api/rest/studies/{study_id}/global-descriptors?job={job_id}", headers=headers, timeout=None)
+        
         ids = {}
 
-        # pdb.set_trace()
-        for item in job_output["globalDescriptors"]["edges"]:
-            item_data = item["node"]
+        for item in resp.json():
+            ids[item['descriptor']] = item 
 
         return ids
